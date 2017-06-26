3d12a8bf73969e058ac7fff2af873b6e9c315245
sharmaraghav260 <sharmaraghav260@gmail.com>
2017-06-25 20:28:01 -0600

    Day 11 done

---
+++ Day11/2dArray.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int arr[][] = new int[6][6];
+        int max = Integer.MIN_VALUE;
+        for(int i=0; i < 6; i++){
+            for(int j=0; j < 6; j++){
+                arr[i][j] = in.nextInt();
+            }
+        }
+        for(int k=0; k < 4; k++){
+            for(int l=0; l < 4; l++){
+                int sum = arr[k][l]+arr[k][l+1]+arr[k][l+2]+arr[k+1][l+1]+arr[k+2][l]+arr[k+2][l+1]+arr[k+2][l+2];
+                if(sum>max)
+                    max = sum;
+            }
+        }
+        System.out.println(max);
+    }
+}
