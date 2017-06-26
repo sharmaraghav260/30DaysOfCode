f989092eb3bbff724ebf35298172b4ce091e71a2
sharmaraghav260 <sharmaraghav260@gmail.com>
2017-06-25 20:08:08 -0600

    Day 10 done

---
+++ Day10/binary.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int rem=0,s=0,t=0;
+
+        while(n>0){
+            rem=n%2;
+            n=n/2;
+            if(rem==1){
+                s++;
+                if(s>=t)
+                    t=s;
+            }
+            else {
+            s=0;
+            }
+        }
+        System.out.println(t);
+    }
+}
